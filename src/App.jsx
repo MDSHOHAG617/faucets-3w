c9e8d8c08ae72d0992ff7d7e799416b39f7a2efa
Md Shohag <mdshohag119764@gmail.com>
2023-08-15 15:58:15 +0600

    done!

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+
+import "./App.css";
+import Footer from "./Components/Footer";
+import MainBody from "./Components/MainBody";
+import MainNavbar from "./Components/MainNavbar";
+import Notice from "./Components/Notice";
+
+function App() {
+  return (
+    <main className="">
+      <div>
+        <MainNavbar />
+        <Notice />
+        <MainBody />
+        <Footer />
+      </div>
+    </main>
+  );
+}
+
+export default App;
